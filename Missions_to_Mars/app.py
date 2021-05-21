ac9bdae48a591f11d1650ff763f9c185790af612
nmbrouss <nicolebroussard@Nicoles-MacBook-Air.local>
2021-05-21 14:17:04 -0400

    Created /scrape route in app.py

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -9,3 +9,21 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_database")
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
+
+
+ # Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_data = scrape_mars.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  
